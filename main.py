2d8ee20d1956513a3775211f55bfd13f608c39cd
arunPdl02 <aroon.paudel@gmail.com>
2026-01-28 14:11:32 -0800

    allowing user to enter the prompt in the command line

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-if api_key == None:
-    raise RuntimeError("Gemini API Key Not found!")
-
 from google import genai
+import argparse
 
-client = genai.Client(api_key= api_key)
 
 
 def main():
-    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    user_prompt = args.user_prompt
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("Gemini API Key Not found!")
+
+    client = genai.Client(api_key= api_key)
     response = client.models.generate_content(
         model ='gemini-2.5-flash',
         contents = user_prompt
         )
+
     token_metadata = response.usage_metadata
-    if token_metadata == None:
+    if not token_metadata:
         raise RuntimeError("failed API request, no metadata recieved")
+    
     response_text = response.text
     prompt_token_count = token_metadata.prompt_token_count
     response_token_count = token_metadata.candidates_token_count
@@ -30,6 +35,7 @@ def main():
     print(f"Response tokens: {response_token_count}")
     print("Response:")
     print(response_text)
+
 
 
 if __name__ == "__main__":
