95ffd15567e6aeb1f56a52bee71efe4b83f182bc
arunPdl02 <aroon.paudel@gmail.com>
2026-01-28 13:57:07 -0800

    adding token metadata information from the api response

--- main.py
+++ main.py
@@ -13,11 +13,23 @@ client = genai.Client(api_key= api_key)
 
 
 def main():
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
         model ='gemini-2.5-flash',
-        contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents = user_prompt
         )
-    print(response.text)
+    token_metadata = response.usage_metadata
+    if token_metadata == None:
+        raise RuntimeError("failed API request, no metadata recieved")
+    response_text = response.text
+    prompt_token_count = token_metadata.prompt_token_count
+    response_token_count = token_metadata.candidates_token_count
+
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {prompt_token_count}")
+    print(f"Response tokens: {response_token_count}")
+    print("Response:")
+    print(response_text)
 
 
 if __name__ == "__main__":
