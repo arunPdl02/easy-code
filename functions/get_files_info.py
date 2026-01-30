0315628e72572b4c5448d7c0c687519ca55241ae
arunPdl02 <aroon.paudel@gmail.com>
2026-01-29 22:41:59 -0800

    added a calculator function and a way to get files

---
+++ functions/get_files_info.py
@@ -0,0 +1,22 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        # Will be True or False
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        file_list = []
+        for file in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, file)
+            file_size = os.path.getsize(file_path)
+            is_file_dir = os.path.isdir(file_path)
+            file_list.append(
+                f"- {file}: file_size={file_size} bytes, is_dir={is_file_dir}")
+        return "\n".join(file_list)
+    except Exception as err:
+        return f"Error listing files:{err}"
