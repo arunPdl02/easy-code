0fa03321d4b2889eb6d88ef48543f1e8ca911fec
arunPdl02 <aroon.paudel@gmail.com>
2026-01-30 13:07:44 -0800

    feature: read the contents of a given file

---
+++ test_get_file_content.py
@@ -0,0 +1,42 @@
+from functions.get_file_content import get_file_content
+
+def test():
+    result = get_file_content("calculator", "lorem.txt")
+    print("Result for lorem.txt")
+    print(len(result))
+    print(result[-100:])
+
+    print("\n")
+
+    result = get_file_content("calculator", "main.py")
+    print("Result for main.py")
+    print(len(result))
+    print(result)
+
+    print("\n")
+
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for pkg/calculator.py")
+    print(len(result))
+    print(result)
+
+    print("\n")
+
+
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for /bin/cat")
+    print(len(result))
+    print(result)
+
+    print("\n")
+
+
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    print("Result for pkg/does_not_exist.py")
+    print(len(result))
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
